947d631565eed0c48ce5d3992163047d44641a37
prasbeck <ch20b083@smail.iitm.ac.in>
2023-03-23 16:49:37 +0530

    image frontend

--- App/frontend/baseUrl.js
+++ App/frontend/baseUrl.js
@@ -1,3 +1,3 @@
-export default "https://edtech.onrender.com";
+// export default "https://edtech.onrender.com";
 // export default "http://192.168.1.6:8000";
-// export default "http://10.22.20.168:8000";
+export default "http://10.22.20.168:8000";
--- App/frontend/screens/Chats.js
+++ App/frontend/screens/Chats.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useRef, useContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useContext,
+  useLayoutEffect,
+} from "react";
 import {
   View,
   Text,
@@ -8,11 +14,14 @@ import {
   ScrollView,
   KeyboardAvoidingView,
   Platform,
+  Button,
+  Image,
 } from "react-native";
 import { io } from "socket.io-client";
 import axios from "axios";
 import baseUrl from "../baseUrl";
 import CustomHeader from "../components/CustomHeader";
+import * as ImagePicker from "expo-image-picker";
 import Loader from "../components/Loader";
 import AuthContext from "../auth/context";
 import { format } from "timeago.js";
@@ -28,6 +37,7 @@ const ChatsScreen = () => {
   const [receiveMessage, setRecieveMessage] = useState(null);
   const [len, setLen] = useState(0);
   const [memCount, setMemCount] = useState();
+  const [image, setImage] = useState();
 
   const socket = useRef();
   const scrollRef = useRef();
@@ -66,7 +76,29 @@ const ChatsScreen = () => {
       })
       .catch((e) => console.log(e));
   }
-  useEffect(() => {
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      // aspect: [4, 3],
+      quality: 1,
+    });
+
+    // console.log(result);
+    if (result.canceled) {
+      // Handle cancellation...
+      return;
+    }
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+      setMessages([
+        ...messages,
+        { senderId: user._id, uri: result.assets[0].uri, name: user.fullName },
+      ]);
+    }
+  };
+
+  useLayoutEffect(() => {
     fetchUser();
     fetchCounts();
   }, []);
@@ -74,6 +106,16 @@ const ChatsScreen = () => {
     setId(user._id);
     fetchMessages();
   }, [user]);
+
+  useEffect(() => {
+    (async () => {
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (status !== "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!");
+      }
+    })();
+  }, []);
 
   useEffect(() => {
     socket.current = io(baseUrl);
@@ -133,7 +175,7 @@ const ChatsScreen = () => {
 
   const MessageSent = ({ msg }) => {
     return (
-      <View style={styles.messageSent}>
+      <View>
         <View
           style={{
             minWidth: "55%",
@@ -144,16 +186,24 @@ const ChatsScreen = () => {
             paddingVertical: 4,
           }}
         >
-          <Text
-            style={{
-              color: "#fff",
-              fontSize: 16,
-              marginHorizontal: 6,
-              marginVertical: 4,
-            }}
-          >
-            {msg.text}
-          </Text>
+          {msg.uri ? (
+            <Image
+              source={{ uri: msg.uri }}
+              style={{ width: 250, height: 200, borderRadius: 10 }}
+              // resizeMode="cover"
+            />
+          ) : (
+            <Text
+              style={{
+                color: "#fff",
+                fontSize: 16,
+                marginHorizontal: 6,
+                marginVertical: 4,
+              }}
+            >
+              {msg.text}
+            </Text>
+          )}
           <Text style={{ color: "#fff", textAlign: "right", fontSize: 10 }}>
             {format(msg.createdAt)}
           </Text>
@@ -164,7 +214,7 @@ const ChatsScreen = () => {
 
   const MessageRecieved = ({ msg }) => {
     return (
-      <View style={styles.messageRecieved}>
+      <View>
         <View
           style={{
             minWidth: "60%",
@@ -176,9 +226,17 @@ const ChatsScreen = () => {
         >
           <Text style={styles.name}>{msg.name}</Text>
 
-          <Text style={[styles.messageText, { color: "black" }]}>
-            {msg.text}
-          </Text>
+          {msg.uri ? (
+            <Image
+              source={{ uri: msg.uri }}
+              style={{ width: 250, height: 200, borderRadius: 10 }}
+              // resizeMode="cover"
+            />
+          ) : (
+            <Text style={[styles.messageText, { color: "black" }]}>
+              {msg.text}
+            </Text>
+          )}
 
           <Text style={{ color: "#3C4048", textAlign: "right", fontSize: 10 }}>
             {format(msg.createdAt)}
@@ -213,12 +271,23 @@ const ChatsScreen = () => {
               {messages.map((data, index) => (
                 <View key={index} style={styles.message}>
                   {data.senderId === id ? (
-                    <MessageSent msg={data} />
+                    <View style={styles.messageSent}>
+                      <MessageSent msg={data} />
+                    </View>
                   ) : (
-                    <MessageRecieved msg={data} />
+                    <View style={styles.messageRecieved}>
+                      <MessageRecieved msg={data} />
+                    </View>
                   )}
                 </View>
               ))}
+              <View
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              ></View>
             </ScrollView>
             <View style={styles.inputContainer}>
               <TextInput
@@ -227,10 +296,17 @@ const ChatsScreen = () => {
                 onChangeText={(text) => setMessage(text)}
                 placeholder="Type your message here"
               />
-              <TouchableOpacity style={styles.button} onPress={sendMessage}>
-                {/* <Text style={styles.buttonText}>Send</Text> */}
-                <Ionicons name="send-sharp" color="#fff" size={20} />
-              </TouchableOpacity>
+              {message ? (
+                <TouchableOpacity style={styles.button} onPress={sendMessage}>
+                  {/* <Text style={styles.buttonText}>Send</Text> */}
+                  <Ionicons name="send-sharp" color="#fff" size={20} />
+                </TouchableOpacity>
+              ) : (
+                <TouchableOpacity style={styles.button} onPress={pickImage}>
+                  {/* <Text style={styles.buttonText}>Send</Text> */}
+                  <Ionicons name="camera-outline" color="#fff" size={20} />
+                </TouchableOpacity>
+              )}
             </View>
           </View>
         </KeyboardAvoidingView>
