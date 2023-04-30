edddc1608e7504f05d306a18fbd2542654e49a3e
prateek-srivastav <prateek.srvastav@gmail.com>
2023-05-01 02:05:45 +0530

    added  notifications

--- App/frontend/screens/Chats.js
+++ App/frontend/screens/Chats.js
@@ -63,11 +63,11 @@ const ChatsScreen = ({ navigation, route }) => {
   const [image, setImage] = useState();
   const [uploading, setUploading] = useState(false);
   const [visible, setVisible] = useState(false);
-  const [sendMessage, setSendMessage] = useState();
-
   const socket = useRef();
   const scrollRef = useRef();
   const inputRef = useRef();
+
+  // const [sendMessage, setSendMessage] = useState();
 
   useEffect(() => {
     setTabBarVisible(false);
@@ -106,6 +106,7 @@ const ChatsScreen = ({ navigation, route }) => {
       socket.current.emit("leaveGroup", group);
     };
   }, [Id]);
+
   useEffect(() => {
     if (receiveMessage) {
       setMessages([...messages, receiveMessage]);
@@ -135,6 +136,7 @@ const ChatsScreen = ({ navigation, route }) => {
         });
     }
   }
+
   useFocusEffect(
     useCallback(() => {
       fetchMessages();
@@ -289,6 +291,153 @@ const ChatsScreen = ({ navigation, route }) => {
     }
   };
 
+  const sendMessage = async () => {
+    if (message) {
+      const res = await axios
+        .post(
+          `${baseUrl}/message`,
+          {
+            text: message,
+            replyOn: replyMessage._id ? replyMessage : {},
+            group,
+          },
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        )
+        .catch((e) => console.log(e));
+      if (!res.data.text) {
+        return alert("something went wrong please try again !");
+      } else {
+        // console.log(res);
+        setReplyMessage({});
+        socket.current.emit("message", { message: res.data, group });
+        setMessage("");
+      }
+    }
+  };
+
+  const MessageSent = ({
+    msg,
+    setSendMessage,
+    message,
+    group,
+    setReplyMessage,
+    setMessage,
+    socket,
+  }) => {
+    const { Id, token } = useContext(AuthContext);
+    const { replyMessage } = useContext(ChatContext);
+
+    const ChatMessage = ({ message }) => {
+      const regex = /(https?:\/\/[^\s]+)/g;
+
+      const textParts = message.split(regex);
+      // console.log(textParts);
+
+      return (
+        <Text
+          style={{
+            color: "#000",
+            fontSize: 16,
+            marginHorizontal: 6,
+            marginVertical: 4,
+          }}
+        >
+          {textParts.map((part, i) => {
+            if (part.match(regex)) {
+              return (
+                <Text
+                  key={i}
+                  style={{ color: "blue" }}
+                  onPress={() => Linking.openURL(part)}
+                >
+                  {part}
+                </Text>
+              );
+            }
+            return <Text key={i}>{part}</Text>;
+          })}
+        </Text>
+      );
+    };
+
+    return (
+      <View style={{ marginBottom: 6, borderRadius: 10 }}>
+        {msg.replyOn?.name && (
+          <View
+            style={{
+              minWidth: "55%",
+              maxWidth: "95%",
+              backgroundColor: "#DEF6D4",
+              borderRadius: 12,
+              borderBottomRightRadius: 0,
+              borderBottomLeftRadius: 0,
+            }}
+          >
+            <View
+              style={{
+                borderLeftColor: "cyan",
+                borderLeftWidth: 4,
+                // backgroundColor: "#D6E4E5",
+                borderRadius: 8,
+                padding: 4,
+              }}
+            >
+              <View>
+                <Text style={{ color: "#5837D0", fontWeight: "600" }}>
+                  {msg.replyOn.senderId == Id ? "you" : msg.replyOn.name}
+                </Text>
+              </View>
+              {msg.replyOn.uri ? (
+                <>
+                  {msg.replyOn.uri.split(".").slice(-1)[0] == "pdf" ? (
+                    <PDFViewer
+                      url={msg.replyOn.uri}
+                      name={msg.replyOn.pdfName}
+                    />
+                  ) : (
+                    <FullscreenImage imageSource={msg.replyOn.uri} />
+                  )}
+                </>
+              ) : (
+                <ChatMessage message={msg.replyOn.text} />
+                // <Text style={{ fontSize: 16 }}>{msg.replyOn.text}</Text>
+              )}
+            </View>
+          </View>
+        )}
+        <View
+          style={{
+            minWidth: "55%",
+            maxWidth: "95%",
+            backgroundColor: "#E7FFDD",
+            borderRadius: 12,
+            paddingHorizontal: 10,
+            paddingVertical: 4,
+            borderTopRightRadius: msg.replyOn?.name ? 0 : 12,
+            borderTopLeftRadius: msg.replyOn?.name ? 0 : 12,
+          }}
+        >
+          {msg.uri ? (
+            <>
+              {msg.uri.split(".").slice(-1)[0] == "pdf" ? (
+                <PDFViewer url={msg.uri} name={msg.pdfName} />
+              ) : (
+                <FullscreenImage imageSource={msg.uri} />
+              )}
+            </>
+          ) : (
+            <ChatMessage message={msg.text} />
+          )}
+          <Text style={{ color: "#000", textAlign: "right", fontSize: 10 }}>
+            {format(msg.createdAt)}
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
   const ChatMessage = ({ message }) => {
     const regex = /(https?:\/\/[^\s]+)/g;
 
@@ -411,6 +560,8 @@ const ChatsScreen = ({ navigation, route }) => {
         imgUri={imgUri}
         navigation={navigation}
         setTabBarVisible={setTabBarVisible}
+        mute
+        group={group}
       />
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -451,10 +602,10 @@ const ChatsScreen = ({ navigation, route }) => {
                   data={data}
                   Id={Id}
                   MessageRecieved={MessageRecieved}
-                  // MessageSent={MessageSent}
+                  MessageSent={MessageSent}
                   message={message}
                   group={group}
-                  setSendMessage={setSendMessage}
+                  // setSendMessage={setSendMessage}
                   setMessage={setMessage}
                   socket={socket}
                 />
